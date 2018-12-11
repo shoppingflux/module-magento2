20c8f633d38c65599107ae23b951075667690409
blmage <bl.mage.fr@gmail.com>
2018-12-11 15:29:54 +0100

    Add the listing of order logs to the sales menu

---
+++ Controller/Adminhtml/Marketplace/Order/Log/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ShoppingFeed\Manager\Controller\Adminhtml\Marketplace\Order\Log;
+
+use ShoppingFeed\Manager\Controller\Adminhtml\Marketplace\Order\LogAction;
+
+class Index extends LogAction
+{
+    public function execute()
+    {
+        return $this->initPage();
+    }
+}
---
+++ Model/ResourceModel/Marketplace/Order/Log/Grid/Collection.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ShoppingFeed\Manager\Model\ResourceModel\Marketplace\Order\Log\Grid;
+
+use Magento\Framework\Api\Search\AggregationInterface as SearchAggregationInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Document as UiDocument;
+use ShoppingFeed\Manager\Model\ResourceModel\Marketplace\Order\Log as LogResource;
+use ShoppingFeed\Manager\Model\ResourceModel\Marketplace\Order\Log\Collection as LogCollection;
+
+class Collection extends LogCollection implements SearchResultInterface
+{
+    /**
+     * @var SearchAggregationInterface
+     */
+    protected $aggregations;
+
+    protected function _construct()
+    {
+        $this->_init(UiDocument::class, LogResource::class);
+    }
+
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+        return $this;
+    }
+
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria)
+    {
+        return $this;
+    }
+
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+}
