e691ffd7d69c56131b26117be61ed888c86d8cb3
blmage <bl.mage.fr@gmail.com>
2020-09-23 17:42:55 +0200

    Fix compatibility with Magento 2.1.18 and 2.2.9

--- Ui/DataProvider/Meta/Compatibility/Fixer.php
+++ Ui/DataProvider/Meta/Compatibility/Fixer.php
@@ -56,21 +56,36 @@ class Fixer
     }
 
     /**
+     * @return bool
+     */
+    private function hasAlwaysTrueDisableTmplInstructions()
+    {
+        $magentoVersion = $this->appMetadata->getVersion();
+
+        /**
+         * The value of "__disableTmpl" instructions is not taken into account by Magento 2.1.18 / 2.2.9 / 2.3.2:
+         * https://github.com/magento/magento2/blob/2.1.18/lib/web/mage/utils/template.js#L56
+         * https://github.com/magento/magento2/blob/2.2.9/lib/web/mage/utils/template.js#L56
+         * https://github.com/magento/magento2/blob/2.3.2/lib/web/mage/utils/template.js#L56
+         *
+         * This was fixed in Magento 2.2.10 / 2.3.3:
+         * https://github.com/magento/magento2/blob/2.2.10/lib/web/mage/utils/template.js#L60
+         * https://github.com/magento/magento2/blob/2.3.3/lib/web/mage/utils/template.js#L60
+         */
+        return (version_compare($magentoVersion, '2.1.18') >= 0)
+            && (version_compare($magentoVersion, '2.1.19') < 0)
+            || (version_compare($magentoVersion, '2.2.9') >= 0)
+            && (version_compare($magentoVersion, '2.2.10') < 0)
+            || (version_compare($magentoVersion, '2.3.2') >= 0)
+            && (version_compare($magentoVersion, '2.3.3') < 0);
+    }
+
+    /**
      * @param array $configuration
      */
     public function fixMetaConfiguration(array $configuration)
     {
-        if (
-            (version_compare($this->appMetadata->getVersion(), '2.3.2') >= 0)
-            && (version_compare($this->appMetadata->getVersion(), '2.3.3') < 0)
-        ) {
-            /**
-             * The value of "__disableTmpl" instructions is not taken into account by Magento 2.3.2:
-             * https://github.com/magento/magento2/blob/2.3.2/lib/web/mage/utils/template.js#L56
-             *
-             * This was fixed in Magento 2.3.3:
-             * https://github.com/magento/magento2/blob/2.3.3/lib/web/mage/utils/template.js#L60
-             */
+        if ($this->hasAlwaysTrueDisableTmplInstructions()) {
             $this->removeSwitcherRulesDisableTmplInstructions($configuration);
         }
 
