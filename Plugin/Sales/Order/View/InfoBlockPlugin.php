984e61eb65debd0d2e030a8d40d63502ea59ed50
blmage <bl.mage.fr@gmail.com>
2022-11-29 11:52:13 +0100

    Add marketplace order information to the invoice view

---
+++ Plugin/Sales/Order/View/InfoBlockPlugin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ShoppingFeed\Manager\Plugin\Sales\Order\View;
+
+use Magento\Sales\Block\Adminhtml\Order\View\Info as InfoBlock;
+
+class InfoBlockPlugin
+{
+    public function afterToHtml(InfoBlock $subject, $result)
+    {
+        if ($marketplaceBlock = $subject->getLayout()->getBlock('sfm_order_marketplace_info')) {
+            $result .= $marketplaceBlock->toHtml();
+        }
+
+        return $result;;
+    }
+}
