4745aac0728c38664ceb2b245aba84c1d87af95a
blmage <bl.mage.fr@gmail.com>
2023-02-22 12:00:06 +0100

    Bump module version

--- Model/Feed/Exporter.php
+++ Model/Feed/Exporter.php
@@ -25,7 +25,7 @@ use ShoppingFeed\Manager\Model\ResourceModel\Feed\Exporter as ExporterResource;
 
 class Exporter
 {
-    const MODULE_VERSION = '1.3.1';
+    const MODULE_VERSION = '1.4.0';
 
     const CHILDREN_EXPORT_MODE_NONE = 'none';
     const CHILDREN_EXPORT_MODE_SEPARATELY = 'separately';
