738d3f6b09d35dcb720f072abc8f0df227f7da6e
blmage <bl.mage.fr@gmail.com>
2024-07-15 10:11:09 +0200

    Improve handling of marketplace order discounts

--- Plugin/SalesRule/ValidatorPlugin.php
+++ Plugin/SalesRule/ValidatorPlugin.php
@@ -3,6 +3,7 @@
 namespace ShoppingFeed\Manager\Plugin\SalesRule;
 
 use Magento\Quote\Model\Quote\Address;
+use Magento\Rule\Model\ResourceModel\Rule\Collection\AbstractCollection as AbstractRuleCollection;
 use Magento\SalesRule\Model\Rule;
 use Magento\SalesRule\Model\Validator;
 use ShoppingFeed\Manager\Model\SalesRule\Rule\Action\Discount\Marketplace\CartFixed as MarketplaceCartFixed;
@@ -36,20 +37,20 @@ class ValidatorPlugin
     public function aroundInitTotals(Validator $subject, callable $proceed, $items, Address $address)
     {
         if (!$items) {
-            return $subject;
+            return $proceed($items, $address);
         }
 
         try {
             $getRulesMethod = new \ReflectionMethod(Validator::class, '_getRules');
             $getRulesMethod->setAccessible(true);
             $rules = $getRulesMethod->invoke($subject, $address);
-        } catch (\ReflectionException $e) {
+        } catch (\Exception $e) {
             $rules = $subject->getRules($address);
         }
 
-        if (is_array($rules)) {
+        if ($rules instanceof AbstractRuleCollection) {
             /** @var Rule $rule */
-            foreach ($subject->getRules($address) as $rule) {
+            foreach ($rules->getItems() as $rule) {
                 if ($rule->getSimpleAction() === MarketplaceCartFixed::ACTION_CODE) {
                     $this->sfmCartFixedRules[] = $rule;
                     // Make sure that item totals are initialized (necessary to reuse the "cart_fixed" behavior later).
