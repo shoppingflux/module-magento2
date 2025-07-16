a7681b557ea1e40a328cff88d3274a2c24155aad
blmage <bl.mage.fr@gmail.com>
2025-07-16 09:48:36 +0200

    Fix compatibility with Magento 2.4.8

--- Model/SalesRule/Rule/Action/Discount/Marketplace/CartFixed.php
+++ Model/SalesRule/Rule/Action/Discount/Marketplace/CartFixed.php
@@ -2,6 +2,7 @@
 
 namespace ShoppingFeed\Manager\Model\SalesRule\Rule\Action\Discount\Marketplace;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Pricing\PriceCurrencyInterface;
 use Magento\Quote\Model\Quote\Item\AbstractItem;
 use Magento\SalesRule\Model\DeltaPriceRound;
@@ -9,6 +10,7 @@ use Magento\SalesRule\Model\Rule;
 use Magento\SalesRule\Model\Rule\Action\Discount\CartFixed as BaseCartFixed;
 use Magento\SalesRule\Model\Rule\Action\Discount\Data as DiscountData;
 use Magento\SalesRule\Model\Rule\Action\Discount\DataFactory as DiscountDataFactory;
+use Magento\SalesRule\Model\Rule\Action\Discount\ExistingDiscountRuleCollector;
 use Magento\SalesRule\Model\Validator;
 use ShoppingFeed\Manager\Model\Sales\Order\ImporterInterface as SalesOrderImporterInterface;
 
@@ -29,7 +31,13 @@ class CartFixed extends BaseCartFixed
         SalesOrderImporterInterface $salesOrderImporter
     ) {
         $this->salesOrderImporter = $salesOrderImporter;
-        parent::__construct($validator, $discountDataFactory, $priceCurrency, $deltaPriceRound);
+
+        /**
+         * Avoid the parent constructor call integrity check.
+         * Because the required parameters change depending on the Magento version,
+         * it is not possible to ensure that only the call relevant to the current version is checked.
+         */
+        $this->callParentConstructor($validator, $discountDataFactory, $priceCurrency, $deltaPriceRound);
     }
 
     /**
@@ -50,5 +58,66 @@ class CartFixed extends BaseCartFixed
         }
 
         return $this->discountFactory->create();
+    }
+
+    /**
+     * @param Validator $validator
+     * @param DiscountDataFactory $discountDataFactory
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param DeltaPriceRound $deltaPriceRound
+     * @return void
+     */
+    private function callParentConstructor(
+        Validator $validator,
+        DiscountDataFactory $discountDataFactory,
+        PriceCurrencyInterface $priceCurrency,
+        DeltaPriceRound $deltaPriceRound
+    ) {
+        if (static::isMagentoVersionAtLeast248()) {
+            parent::__construct(
+                $validator,
+                $discountDataFactory,
+                $priceCurrency,
+                $deltaPriceRound,
+                ObjectManager::getInstance()->get(ExistingDiscountRuleCollector::class)
+            );
+        } else {
+            parent::__construct(
+                $validator,
+                $discountDataFactory,
+                $priceCurrency,
+                $deltaPriceRound
+            );
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private static function isMagentoVersionAtLeast248(): bool
+    {
+        static $hasCollectorParam = null;
+
+        if (null !== $hasCollectorParam) {
+            return $hasCollectorParam;
+        }
+
+        try {
+            $hasCollectorParam = false;
+
+            $class = new \ReflectionClass(BaseCartFixed::class);
+            $params = $class->getConstructor()?->getParameters() ?? [];
+
+            foreach ($params as $param) {
+                if ($param->getName() === 'existingDiscountRuleCollector') {
+                    $hasCollectorParam = true;
+                    break;
+                }
+            }
+        } catch (\ReflectionException $e) {
+            $hasCollectorParam = false;
+        }
+
+        return $hasCollectorParam;
     }
 }
